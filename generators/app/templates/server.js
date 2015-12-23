56308f5df35b2c7472ef14b50d36923528222ee9
Timothy Moran <tyrsius@gmail.com>
2015-12-23 23:04:10 +0000

    add build js script

--- generators/app/templates/server.js
+++ generators/app/templates/server.js
@@ -1,6 +1,7 @@
 var port = process.env.PORT || 9000,
     ip = process.env.IP || "0.0.0.0",
-    clientDir = __dirname + '/src/',
+    isProduction = process.env.NODE_ENV == 'production',
+    clientDir = __dirname + (isProduction ? '/dist/' : '/src/'),
     assetDir = __dirname + '/assets',
     jspmConfigName = '/system.config.js',
     jspmConfig = __dirname + jspmConfigName,
@@ -17,14 +18,10 @@ app.get(jspmConfigName, function(req, res) {
 	res.sendFile(jspmConfig);
 });
 
-
 //Index Route
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + (isProduction ? '/index-built.html' : '/index.html'));
 });
-
-//For multi-hosting app
-//module.exports =  app;
 
 //Start Listening
 app.listen(port, ip);
